3551e132446b38e6edf3dcdefe4ebf87cfa7bd80
KotKHPI <prototype2428@gmail.com>
2020-10-29 10:36:56 -0400

    Remake

---
+++ lab04/1/src/main.c
@@ -0,0 +1,14 @@
+#define R 5
+#define PI 3.14
+int main()
+{
+	char command = 'l';
+	float result;
+switch(command) {
+	case 'l' : 2 * PI * R; break;
+	case 's' : PI * R * R; break;
+	case 'v' : 4/3 * PI* R * R  * R; break;
+	default : 5;
+}
+	return 0;
+}
