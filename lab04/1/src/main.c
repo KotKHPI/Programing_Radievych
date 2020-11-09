dea3b836cbb6ac908a7667261d77e43333455fdf
KotKHPI <prototype2428@gmail.com>
2020-11-09 13:28:25 -0500

    more info

--- lab04/1/src/main.c
+++ lab04/1/src/main.c
@@ -2,12 +2,12 @@
 #define PI 3.14
 int main()
 {
-	char command = 'l';
+	char command = 'l';  // вибір команди
 	float result;
 switch(command) {
-	case 'l' : 2 * PI * R; break;
-	case 's' : PI * R * R; break;
-	case 'v' : 4/3 * PI* R * R  * R; break;
+	case 'l' : 2 * PI * R; break;  //при виборі команди "l" буде виконуватися така дія
+	case 's' : PI * R * R; break; // при виборі команди "s" буде виконуватися така дія
+	case 'v' : 4/3 * PI* R * R  * R; break; // при виборі команди "v" буде виконуватися така дія
 }
 	return 0;
 }
--- lab04/3/src/main.c
+++ lab04/3/src/main.c
@@ -1,20 +1,20 @@
-int main() {
+int main() { // суть завдяння виконання умови k < m < n
 	int k = 3;
 	int m = 2;
 	int n = 1;
-	int temp;
+	int temp; // вільна змінна
 
-	if (k > m) {
+	if (k > m) { // змінемо місцями m i k
 	temp = k;
 	k = m;
 	m = temp;
 }
-	if (k > n) {
+	if (k > n) { // змінемо місцями n i k
 	temp = k;
 	k = n;
 	n = temp;
 }
-	if (m > n) {
+	if (m > n) { // змінемо місцями n i m
 	temp = m;
 	m = n;
 	n = temp;
