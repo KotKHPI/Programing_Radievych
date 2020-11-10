331031915d1b293886f625fc9dc4e3ef2ed29cff
KotKHPI <prototype2428@gmail.com>
2020-11-10 15:33:14 -0500

    lab05

--- lab05/5/src/main.c
+++ lab05/5/src/main.c
@@ -1,4 +1,14 @@
+#define N 16
 int main() {
+ int mult = 0; // множник
+ int root = 0; //корінь
 
+for (int i = 1; i < N; i ++) {
+	mult = i * i; // виделення точного множника з числа N
+	if (mult == N) {
+	 mult = i; // множник числа N
+	 root = N / mult; // знаходження корня числа N
+ }
+}
 	return 0;
 }
