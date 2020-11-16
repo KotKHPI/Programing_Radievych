712e5de4201387131a08c9e8559e5126f8412ffd
KotKHPI <prototype2428@gmail.com>
2020-11-16 08:19:44 -0500

    Lab04

--- lab04/1/src/main.c
+++ lab04/1/src/main.c
@@ -2,12 +2,24 @@
 #define PI 3.14
 int main()
 {
-	char command = 'l';  // вибір команди
+	char command = 's';  // вибір команди
+	float l = 0;
+	float s = 0;
+	float v = 0;
 	float result;
 switch(command) {
-	case 'l' : 2 * PI * R; break;  //при виборі команди "l" буде виконуватися така дія
-	case 's' : PI * R * R; break; // при виборі команди "s" буде виконуватися така дія
-	case 'v' : 4/3 * PI* R * R  * R; break; // при виборі команди "v" буде виконуватися така дія
+	case 'l' :
+	l = 2 * PI * R;
+	result = l;
+	break;  //при виборі команди "l" буде виконуватися така дія
+	case 's' :
+	s = PI * R * R;
+	result = s;
+	break; // при виборі команди "s" буде виконуватися така дія
+	case 'v' :
+	v = 4/3 * PI* R * R  * R;
+	result = v;
+	break; // при виборі команди "v" буде виконуватися така дія
 }
 	return 0;
 }
