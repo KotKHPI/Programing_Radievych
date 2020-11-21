e2ce20830ea26145c458122490b7bdab4815b733
KotKHPI <prototype2428@gmail.com>
2020-11-21 05:56:40 -0500

    Дії з командами

--- lab04/1/src/main.c
+++ lab04/1/src/main.c
@@ -3,21 +3,18 @@
 int main()
 {
 	char command = 's';  // вибір команди
-	float l = 0;
-	float s = 0;
-	float v = 0;
 	float result;
 switch(command) {
 	case 'l' :
-	l = 2 * PI * R;
+	float l = 2 * PI * R;
 	result = l;
 	break;  //при виборі команди "l" буде виконуватися така дія
 	case 's' :
-	s = PI * R * R;
+	float s = PI * R * R;
 	result = s;
 	break; // при виборі команди "s" буде виконуватися така дія
 	case 'v' :
-	v = 4/3 * PI* R * R  * R;
+	float v = 4/3 * PI* R * R  * R;
 	result = v;
 	break; // при виборі команди "v" буде виконуватися така дія
 }
