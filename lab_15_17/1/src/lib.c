274af14368713f2e3382b93d5a2a01e921994cca
KotKHPI <prototype2428@gmail.com>
2021-03-14 23:50:27 +0200

    Lab 15

--- lab_15_17/1/src/lib.c
+++ lab_15_17/1/src/lib.c
@@ -3,7 +3,7 @@
  * @brief Файл з реалізацією функцій
  *
  * @author Radievych V.
- * @date 22-dec-2020
+ * @date 9-mar-2021
  * @version 1.0
  */
 
@@ -14,5 +14,103 @@ void check_file(FILE *fmatrix) {
         printf("Файл не знайдено\n");
         printf("%d\n\n", errno);
         exit(1);
+    }
+}
+
+void scanning (Basic* p_bird, int count_birds, FILE *birds_info) {
+    int i = 0;
+    while (i != count_birds) {
+        fscanf(birds_info, "%d %s %d %d %d %d %d %d", &(p_bird + i)->label, (p_bird + i)->name, &(p_bird + i)->age, &(p_bird + i)->home.square, &(p_bird + i)->home.height, &(p_bird + i)->home.number_of_feeders, &(p_bird + i)->home.nest_nest, &(p_bird + i)->sex);
+        i++;
+    }
+}
+
+void sort_by_age (Basic* p_bird, int count_birds) {
+    for(int i = 0 ; i < count_birds - 1; i++) {
+        if((p_bird + i)->age > (p_bird + i + 1)->age) {
+            Basic tmp = *(p_bird + i);
+            *(p_bird + i) = *(p_bird + i + 1);
+            *(p_bird + i + 1) = tmp;
+        }
+    }
+}
+
+void output_file (Basic* p_bird, int count_birds, FILE* out_info) {
+    int j = 0;
+    while(j != count_birds) {
+        fprintf(out_info, "Птах № %d\n\n", j + 1);
+        
+        fprintf(out_info, "Чи окольцована птаха: ");
+        if ((p_bird + j)->label == 0) {
+            fprintf(out_info, "Так\n");
+        } else {
+            fprintf(out_info, "Ні\n");
+        }
+        
+        fprintf(out_info, "Назва птаха: %s\nВік птаха: %d місяців\nПлоща домівки: %d см^2\nВисота домівки: %d см.\nКількість годівниць: %d\n", (p_bird + j)->name, (p_bird + j)->age, (p_bird + j)->home.square, (p_bird + j)->home.height, (p_bird + j)->home.number_of_feeders);
+        
+        fprintf(out_info, "Наявність гнізда: ");
+        if ((p_bird + j)->home.nest_nest == 0) {
+            fprintf(out_info, "Є гніздо\n");
+        } else {
+            fprintf(out_info, "Немає гнізда\n");
+        }
+        
+        fprintf(out_info, "Стать: ");
+        if ((p_bird + j)->sex == 0) {
+            fprintf(out_info, "Чоловіча\n\n");
+        } else {
+            fprintf(out_info, "Жіноча\n\n");
+        }
+        j++;
+    }
+    printf("Відсортовані данні було виведено у файл!\n\n");
+}
+
+void random_elements (Basic* p_bird, int count_birds) {
+    for(int f = 0; f < count_birds; f++) {
+        int i = rand() % 2;
+        char i_c = i + '0';
+        char tmp[] = "Птах № ";
+        //strcat(tmp, i_c);
+        
+        (p_bird + f)->label = rand() % 2;
+        (p_bird + f)->age = rand() % 208 + 23;
+        (p_bird + f)->home.square = rand() % 208 + 23;
+        (p_bird + f)->home.height = rand() % 208 + 23;
+        (p_bird + f)->home.number_of_feeders = rand() % 208 + 23;
+        (p_bird + f)->home.nest_nest = rand() % 2;
+        (p_bird + f)->sex = rand() % 2;
+    }
+}
+
+void output_monitor (Basic* p_bird, int count_birds) {
+    printf("Виведення на екран випадкових елементів структури:\n\n");
+    
+    for(int j = 0; j < count_birds; j++) {
+        printf("Птах № %d\n\n", j + 1);
+        
+        printf("Чи окольцована птаха: ");
+        if ((p_bird + j)->label == 0) {
+            printf("Так\n");
+        } else {
+            printf("Ні\n");
+        }
+        
+        printf("Назва птаха: %s\nВік птаха: %d місяців\nПлоща домівки: %d см^2\nВисота домівки: %d см.\nКількість годівниць: %d\n", (p_bird + j)->name, (p_bird + j)->age, (p_bird + j)->home.square, (p_bird + j)->home.height, (p_bird + j)->home.number_of_feeders);
+        
+        printf("Наявність гнізда: ");
+        if ((p_bird + j)->home.nest_nest == 0) {
+            printf("Є гніздо\n");
+        } else {
+            printf("Немає гнізда\n");
+        }
+        
+        printf("Стать: ");
+        if ((p_bird + j)->sex == 0) {
+            printf("Чоловіча\n\n");
+        } else {
+            printf("Жіноча\n\n");
+        }
     }
 }
--- lab_15_17/1/src/main.c
+++ lab_15_17/1/src/main.c
@@ -4,7 +4,7 @@
  * 1. **Вирахувати** для тексту частотну таблицю: для кожного символу визначити його частоту появи у тексті.
  *
  * @author Radievych V.
- * @date 22-dec-2020
+ * @date 9-mar-2021
  * @version 1.0
  */
 
@@ -13,77 +13,51 @@
  * @brief Файл з головною функцією main, що задає розмір масивам, змінним, виділяючи їм пам'ять та викликає функції count_text_length, count_of_unique_elements, fill_zeros, get_symbols, get_symbols_counts, get_symbols_frequencies.
  *
  * @author Radievych V.
- * @date 15-dec-2020
+ * @date 9-mar-2021
  * @version 1.0
  */
 
 #include "lib.h"
 
-enum Yes_no {
-    Так, //0
-    Ні //1
-};
-
-enum Sex {
-    Чоловіча,
-    Жіноча
-};
-
-typedef struct {
-    int square; //кв.см
-    int height; //см
-    int number_of_feeders;
-    enum Yes_no nest_nest; //да, нет
-} Feature;
-
-typedef struct {
-    enum Yes_no label; //да, нет
-    char name[15]; //петух, жаравель и тд
-    int age; //месяцов
-    Feature home;
-    enum Sex sex; //мужской, женский
-} Basic;
-
-
 int main() {
-    FILE *birds_info;
-    char name_file[30] = "0";
-    printf("Введіть імя файлу з текстом (шлях до файлу): ");
-    scanf("%s", name_file);
+    srand(time(NULL));
     
-    birds_info = fopen("/Users/whatislove/birds.txt", "r+"); //name_file
+    //Ввод
+    FILE *birds_info;
+    birds_info = fopen("/Users/whatislove/birds.txt", "r"); //name_file
     check_file(birds_info);
+    
+    //Вывод
+    FILE *out_info;
+    out_info = fopen("/Users/whatislove/out_birds.txt", "w");
     
     printf("Кількість птиць: ");
     int count_birds;
     scanf("%d", &count_birds);
     
-    /*struct Basic ** p_bird = malloc(count_birds * sizeof(struct Basic*));
-    for (int i = 0; i < 2000; i++) {
-        *(p_bird + i) = malloc(sizeof(struct Basic*));
-    }*/
+    Basic* p_bird = malloc(count_birds * sizeof(Basic));
     
-    Basic* bird1 = malloc(sizeof(Basic));
-    Basic* bird2 = malloc(sizeof(Basic));
-    Basic* bird3 = malloc(sizeof(Basic));
+    //Сканирование
     
+    scanning(p_bird, count_birds, birds_info);
     
-    fscanf(birds_info, "%d %s %d %d %d %d %d %d", &bird1->label, bird1->name, &bird1->age, &bird1->home.square, &bird1->home.height, &bird1->home.number_of_feeders, &bird1->home.nest_nest, &bird1->sex);
+    //Сортировка за возрастом
     
-    fscanf(birds_info, "%d %s %d %d %d %d %d %d", &bird2->label, bird2->name, &bird2->age, &bird2->home.square, &bird2->home.height, &bird2->home.number_of_feeders, &bird2->home.nest_nest, &bird2->sex);
+    sort_by_age(p_bird, count_birds);
     
-    fscanf(birds_info, "%d %s %d %d %d %d %d %d", &bird3->label, bird3->name, &bird3->age, &bird3->home.square, &bird3->home.height, &bird3->home.number_of_feeders, &bird3->home.nest_nest, &bird3->sex);
+    //Вывод данных в файл
     
-    /*if (bird2->label == 0) {
-        printf("Так ");
-    }
-    if (bird2->label == 1) {
-        printf("Нить ");
-    }
+    output_file(p_bird, count_birds, out_info);
     
-    printf("%s %d %d %d %d %d %d", bird2->name, bird2->age, bird2->home.square, bird2->home.height, bird2->home.number_of_feeders, bird2->home.nest_nest, bird2->sex);*/
+    //Рандом значений
     
-    //free Basic** TODO
+    random_elements(p_bird, count_birds);
+    
+    //Вывод на экран
+    
+    output_monitor(p_bird, count_birds);
+        
     fclose(birds_info);
+    fclose(out_info);
     return 0;
 }
